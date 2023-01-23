f6cb48e3da85bc90b4a88ddb0dbd46638fa30bc6
rithick69 <rithickcy19@gmail.com>
2023-01-23 11:31:55 +0530

    Adding React files used for Learning

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//var React = require('react');
+import React from 'react';
+//var ReactDom = require('react-dom');
+import ReactDom from 'react-dom';
+
+// ReactDom.render('What to show', 'Where to show', 'callback func');
+
+ReactDom.render(<h1>Hello World</h1>, document.getElementById('root'));
+
