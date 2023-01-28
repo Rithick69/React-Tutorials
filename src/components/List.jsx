cb7c086816c3ad1ddd600749369448a10af8ab73
rithick69 <rithickcy19@gmail.com>
2023-01-28 13:30:42 +0530

    Learning about react components

---
+++ src/components/List.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function List(){
+    return(
+        <ol>
+            <li> Modern Family </li>
+            <li> The Office </li>
+            <li> Friends </li>
+            <li> Family Guy </li>
+            <li> How I met your mother </li>
+        </ol>
+    );
+}
+
+export default List;
