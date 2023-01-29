a975ea9408733813aea60db5a0f541a50275308c
rithick69 <rithickcy19@gmail.com>
2023-01-29 21:03:23 +0530

    Learning about import export

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from "react";
-import "./styles.css";
-import C3div from "./components/C3div";
+const myfav = "Thapa";
 
-function App(){
-    return (
-        <C3div/>
-    );
+const favprog = "React js";
+
+function myName() {
+	let name = "vinod";
+	return name;
+}
+function myNames() {
+	let names = "vinod dikon";
+	return names;
 }
 
-export default App;
+export default myfav;
+// default can only have one value
+// we use default when we don't have any other variable or function to export.
+// it quite like the switch case default condition..
+// the name of the variable can be anything while importing in the index page.
+
+export { myName, myNames, favprog };
+
+//the name of the variable needs to be exactly same while importing in the index page.
