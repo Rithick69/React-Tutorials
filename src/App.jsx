f9c39c0c596341dc811f08008b02d84b266fbc87
rithick69 <rithickcy19@gmail.com>
2023-01-30 23:17:12 +0530

    Creating a web page for top 5 shows

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { add, sub, div, mul } from "./components/Calc";
-
-//Calculator
+import Cards from "./components/Cards";
 
 function App() {
 	return (
 		<>
-			<ul>
-				<li>Sum of two no is: {add(40, 4)}</li>
-				<li>Sub of two no is: {sub(30, 4)}</li>
-				<li>Div of two no is: {div(10, 3)}</li>
-				<li>Mult of two no is: {mul(10, 4)}</li>
-			</ul>
+			{/* Making my custom properties and passing values in the component */}
+			<Cards
+				imgsrc="Image1"
+				title="A Netflix Original Series"
+				sname="Dark"
+				link="link1"
+			/>
+			<Cards
+				imgsrc="Image2"
+				title="A Netflix Original Series"
+				sname="Friends"
+				link="link2"
+			/>
+			<Cards
+				imgsrc="Image3"
+				title="A Netflix Original Series"
+				sname="Nobody"
+				link="link3"
+			/>
 		</>
 	);
 }
-
 export default App;
