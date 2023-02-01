814183024019fcc253aac0bc03d9026550346f40
rithick69 <rithickcy19@gmail.com>
2023-02-01 23:57:13 +0530

    Creating a web page for top 5 shows Part 3

--- src/components/Sdata.jsx
+++ src/components/Sdata.jsx
@@ -1,29 +1,34 @@
 const Sdata = [
 	{
+		id: 1,
 		sname: "Stranger Things",
 		title: "A Netflix Original Series",
 		imgsrc: "https://wallpapercave.com/wp/wp1917154.jpg",
 		link: "https://www.netflix.com/gb/title/80057281?source=35",
 	},
 	{
+		id: 2,
 		sname: "My First-2 Love",
 		title: "A Netflix Original Series",
 		imgsrc: "https://flxt.tmsimg.com/assets/p17080748_b_h9_aa.jpg",
 		link: "https://www.netflix.com/gb/title/81026700",
 	},
 	{
+		id: 3,
 		sname: "Extra Curricular",
 		title: "A Netflix Original Series",
 		imgsrc: "https://wallpapercave.com/wp/wp7418473.jpg",
 		link: "https://www.netflix.com/gb/title/80990668?source=35",
 	},
 	{
+		id: 4,
 		sname: "Dark",
 		title: "A Netflix Original Series",
 		imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
 		link: "https://www.netflix.com/gb/title/80100172?source=35",
 	},
 	{
+		id: 5,
 		sname: "The Vampire Diaries",
 		title: "A Netflix Original Series",
 		imgsrc:
