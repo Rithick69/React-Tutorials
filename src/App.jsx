ddf36ef806b043e22f2dfb3c7e32ad5ea132907c
rithick69 <rithickcy19@gmail.com>
2023-02-04 00:47:18 +0530

    Creating a slot machine Part2

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from "react";
+import "./styles.css";
 import SlotM from "./components/Slotmach";
+import Slotdata from "./components/Slotdata";
 
 // Creating a mini "Slot Machine" game
 
 const App = () => (
 	<>
 		<h1 className="heading_style">
-			Welcome to <span style={{ fontWeight: "bold" }}>Slot Machine Game</span>
+			Welcome to{" "}
+			<span style={{ fontWeight: "bold", backgroundColor: "transparent" }}>
+				Slot Machine Game
+			</span>
 		</h1>
 		<div className="slotmachine">
 			{/* Installed emojisense extension for emoji */}
-			<SlotM x=" 😄 " y=" 😄 " z=" 😄 " />
-			<hr />
-			<SlotM x=" 🎅 " y=" 🎅 " z=" 😄 " />
-			<hr />
-			<SlotM x=" 🍎 " y=" 🍎 " z=" 🍎 " />
-			<hr />
-			<SlotM x=" 🍌 " y=" 😆 " z=" 🍎 " />
+
+			{Slotdata.map((val) => {
+				return <SlotM key={val.id} x={val.x} y={val.y} z={val.z} />;
+			})}
 		</div>
 	</>
 );
