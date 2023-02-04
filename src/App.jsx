ee439591a193a1857c932fdef170641cfea2775a
rithick69 <rithickcy19@gmail.com>
2023-02-04 23:17:11 +0530

    Created an auto updating webpage to display time using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+// Mini project to display auto updating time in a webpage
+
+const App = () => {
+	let newTime = new Date().toLocaleTimeString();
+	const [curr, setCount] = useState(newTime);
+	const UpdateTime = () => {
+		let currTime = new Date().toLocaleTimeString();
+		setCount(currTime);
+	};
+
+	// using setInterval property to auto update time after the set waiting time.
+
+	setInterval(UpdateTime, 1000);
+	// 1000ms is 1 sec
+
+	return (
+		<>
+			<h1> {curr} </h1>
+		</>
+	);
+};
+
+// Challenge: 6
+
+// Display the current Time on refresh or button click using Hooks
+
+const App2 = () => {
+	let newTime = new Date().toLocaleTimeString();
+	const [curr, setCount] = useState(newTime);
+	const GetTime = () => {
+		let currTime = new Date().toLocaleTimeString();
+		setCount(currTime);
+	};
+
+	return (
+		<>
+			<h1> {curr} </h1>
+			<button onClick={GetTime}> Click Me</button>
+		</>
+	);
+};
+
 // Learning React Hooks
 // Created a webpage where count is displayed and increments by 1 when user
 // clicks the button
 
-const App = () => {
+const App1 = () => {
 	// Hooks can only be called inside a functional component.
 	//
 	// const state = useState();
