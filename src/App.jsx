37044c2773393231f514ac4b9a0d9b80967f4b03
rithick69 <rithickcy19@gmail.com>
2023-02-08 23:17:21 +0530

    Hooks Challenge Increment Decrement web page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import ToDoCard from "./components/ToDoCard";
 import "./styles.css";
 
-// TODO LIST using ReactJS
+// Hooks Challenge
 
 const App = () => {
+	const [CurrData, setData] = useState(0);
+
+	const Increment = () => {
+		setData((prev) => {
+			return prev + 1;
+		});
+	};
+	const Decrement = () => {
+		setData((prev) => {
+			return prev !== 0 ? prev - 1 : 0;
+		});
+	};
+
+	return (
+		<>
+			<div className="main_div">
+				<div className="center_hooks_div">
+					<br />
+					<h1 className="heading">{CurrData}</h1>
+					<div className="btn_div">
+						<button className="buttons" onClick={Increment}>
+							{" "}
+							+{" "}
+						</button>
+						<button className="buttons" onClick={Decrement}>
+							{" "}
+							-{" "}
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+// TODO LIST using ReactJS
+
+const App1 = () => {
 	return (
 		<>
 			<ToDoCard />
