0cca5ef066c3378cc710e7709be49875d9a6a595
rithick69 <rithickcy19@gmail.com>
2023-02-08 23:11:51 +0530

    Creating a ToDO List using React Part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,13 @@
-import React, { useState } from "react";
-import TodoList from "./components/ToDoList";
+import React from "react";
+import ToDoCard from "./components/ToDoCard";
 import "./styles.css";
 
 // TODO LIST using ReactJS
 
 const App = () => {
-	const [inputList, setInputList] = useState("");
-	// Adding []s to the useState makes the curr variable an array.
-	// Initialising Items as an empty array.
-	const [Items, setItems] = useState([]);
-
-	const itemEvent = (event) => {
-		const { value } = event.target;
-		setInputList(value);
-	};
-
-	// Storing every new item in an empty as well as displaying it
-
-	const listOfItems = () => {
-		setItems((prevItems) => {
-			// Adding the  prev value of the Items array and if array is empty
-			// then push the new value from inputList to the array.
-			// UseState return the a new array which becomes the curr data of Items array.
-
-			return [...prevItems, inputList];
-		});
-		// Clearing the input field after add the item to the array.
-		setInputList("");
-	};
 	return (
 		<>
-			<div className="main_div">
-				<div className="center_div">
-					<br />
-					<h1> ToDo List</h1>
-					<br />
-					<input
-						type="text"
-						placeholder="Add a Item"
-						value={inputList}
-						onChange={itemEvent}
-					/>
-					<button onClick={listOfItems}> + </button>
-
-					<ol>
-						{/* <li>{todoItem}</li> */}
-						{/* Using map method to display the items of the 'Items' array */}
-						{Items.map((val, index) => (
-							<TodoList key={index} text={val} />
-						))}
-					</ol>
-				</div>
-			</div>
+			<ToDoCard />
 		</>
 	);
 };
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,8 +4,19 @@ const TodoList = (props) => {
 	return (
 		<>
 			<div className="todo_style">
-				<i class="fa fa-times" aria-hidden="true" />
-				<li key={props.key}>{props.text}</li>;
+				{/* We need to return the id of the element we want to delete */}
+				<i
+					className="fa fa-times"
+					aria-hidden="true"
+					onClick={() => {
+						props.onSelect(props.id);
+					}}
+				/>
+				{/* Props are immutable which means they can't be changed */}
+				{/* We want delete event to work on App.jsx */}
+				<li key={props.key} id={props.id}>
+					{props.text}
+				</li>
 			</div>
 		</>
 	);
