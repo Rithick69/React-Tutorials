e6186160e43c380e5cd6dab8f9efd7bc16e5063c
rithick69 <rithickcy19@gmail.com>
2023-02-17 23:56:15 +0530

    Added Create note function and displaying them serially

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./components/note/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Note from "./components/note/Note";
@@ -7,16 +7,31 @@ import Footer from "./components/note/Footer";
 import Header from "./components/note/Header";
 // Notes App
 const App = () => {
+	const [addItem, setAddItem] = useState([]);
+	// we use this props to pass the note array to note component.
+	const addNote = (note) => {
+		// Store the note data to array.
+		setAddItem((prev) => {
+			// Since addItem is an array we use [] for return.
+			return [...prev, note];
+		});
+	};
 	return (
 		<>
-			{/* Header Component */}
 			<Header />
-			{/* CreateNote Component */}
-			<CreateNote />
-
-			{/* Note Component */}
-			<Note />
-			{/* Footer Component */}
+			{/* Passing a props for the add note event */}
+			<CreateNote passNote={addNote} />
+			{/* Passing addItem array data to Note component via props */}
+			{addItem.map((val, index) => {
+				return (
+					<Note
+						key={index}
+						id={index}
+						title={val.title}
+						content={val.content}
+					/>
+				);
+			})}
 			<Footer />
 		</>
 	);
--- src/components/note/CreateNote.jsx
+++ src/components/note/CreateNote.jsx
@@ -1,15 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@material-ui/core";
 import { Add } from "@material-ui/icons";
 
-const CreateNote = () => {
+const CreateNote = (props) => {
+	const [note, setNote] = useState({
+		title: "",
+		content: "",
+	});
+
+	// Populate the input field and textarea using onChange event.
+	const InputEvent = (event) => {
+		const { value, name } = event.target;
+		setNote((prev) => {
+			return {
+				...prev,
+				[name]: value,
+			};
+		});
+	};
+
+	// We need to pass the event to the main App component
+	// to be passed further to the Note component.
+	const addEvent = () => {
+		// we call props passnote through here.
+		// send the note data to app component.
+		props.passNote(note);
+		// Auto clearing the input field and textarea after click call
+		setNote({
+			title: "",
+			content: "",
+		});
+	};
 	return (
 		<>
 			<div className="main_notes">
 				<form>
-					<input type="text" placeholder="Title" autoComplete="off" />
-					<textarea rows="" column="" placeholder="Write a note ..."></textarea>
-					<Button>
+					<input
+						type="text"
+						name="title"
+						value={note.title}
+						onChange={InputEvent}
+						placeholder="Title"
+						autoComplete="off"
+					/>
+					<textarea
+						rows=""
+						column=""
+						name="content"
+						value={note.content}
+						onChange={InputEvent}
+						placeholder="Write a note ..."
+					></textarea>
+
+					{/* We need to add each note to an array after click of add button */}
+
+					<Button onClick={addEvent}>
 						<Add className="plus_sign" />
 					</Button>
 				</form>
