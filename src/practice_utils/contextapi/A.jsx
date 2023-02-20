fa084f0898c3e10eb2b41fe6f64cc78ce065ff09
rithick69 <rithickcy19@gmail.com>
2023-02-20 23:59:47 +0530

    Learning abt context api

---
+++ src/practice_utils/contextapi/A.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import B from "./B";
+
+const A = () => {
+	return (
+		<>
+			<B />
+		</>
+	);
+};
+
+export default A;
---
+++ src/practice_utils/contextapi/B.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import C from "./C";
+
+const B = () => {
+	return (
+		<>
+			<C />
+		</>
+	);
+};
+
+export default B;
