9033c52b8db3b3339ba9d5b8a3af612f125aa160
rithick69 <rithickcy19@gmail.com>
2023-02-22 23:58:54 +0530

    Learning about react routers

---
+++ src/practice_utils/react_routers/Contact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contact = () => {
+	return <h1>This is Contact page</h1>;
+};
+
+export default Contact;
---
+++ src/practice_utils/react_routers/Error.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Error = () => {
+	return <h1>This is 404 Error page</h1>;
+};
+
+export default Error;
