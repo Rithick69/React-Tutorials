f6a0bf7583f843d6b9c9b6fe766b3e46c24d3631
rithick69 <rithickcy19@gmail.com>
2023-02-24 00:19:36 +0530

    Learnt about useNavigate hooks in react router

--- src/practice_utils/react_routers/Contact.jsx
+++ src/practice_utils/react_routers/Contact.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 const Contact = () => {
+	const navigate = useNavigate();
+	const goToHome = () => {
+		navigate("/");
+	};
 	return (
 		<>
-			<h1>This is Contact page</h1>
-			<Link to="name">My Name</Link>
-			<Outlet />
+			<section>
+				<h1 style={{ marginTop: "20px" }}>This is Contact page</h1>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "space-between",
+						width: "200px",
+						marginTop: "40px",
+						flexDirection: "column",
+					}}
+				>
+					<button onClick={() => goToHome()}>Go to Home page</button>
+					<br />
+					<Link to="name">My Name</Link>
+					<Outlet />
+				</div>
+			</section>
 		</>
 	);
 };
--- src/practice_utils/react_routers/Home.jsx
+++ src/practice_utils/react_routers/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
 	return (
 		<>
-			<h1>This is Home page</h1>
+			<h1 style={{ margin: "20px 0" }}>This is Home page</h1>
 		</>
 	);
 };
