c2789dffd215f4e2630c83a81aff538a6330a8ef
rithick69 <rithickcy19@gmail.com>
2023-04-16 23:59:01 +0530

    Added speech recog npm pkg to enable speech to text conversion, WIP

--- src/components/speech_to_text/Template.jsx
+++ src/components/speech_to_text/Template.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import SpeechRecognition, {
+	useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
 import "./index.css";
 
 function Template() {
+	const [text, setText] = useState();
+	const [isCopied, setCopied] = useClipboard(text);
+
+	// If you want to listen continuously, set the continuous property to true when calling startListening
+	const startListening = () =>
+		SpeechRecognition.startListening({
+			continuous: true,
+			language: "en-IN",
+		});
+
+	const stopListening = () => SpeechRecognition.stopListening();
+
+	// To make the microphone transcript available in your component, simply add:
+	const { transcript, browserSupportsSpeechRecognition } =
+		useSpeechRecognition();
+
+	// Checks if the browser supports Speech Recognition.
+	if (!browserSupportsSpeechRecognition) {
+		return null;
+	}
+
 	return (
 		<>
 			<div className="container">
@@ -11,11 +36,15 @@ function Template() {
 					A React hook that converts speech fromt the microphone to text and
 					makes it available to your React components.
 				</p>
-				<div className="main-content"></div>
+				<div className="main-content" onClick={() => setText(transcript)}>
+					{transcript}
+				</div>
 				<div className="btn-style">
-					<button>Copy to Clipboard</button>
-					<button>Start Recording</button>
-					<button>Stop Recording</button>
+					<button onClick={setCopied}>
+						{isCopied ? "Copied" : "Copy to Clipboard"}
+					</button>
+					<button onClick={startListening}>Start Recording</button>
+					<button onClick={stopListening}>Stop Recording</button>
 				</div>
 			</div>
 		</>
